d8dbee31b947bef912663e206ce9100299046103
mygfw <101572846+mygfw@users.noreply.github.com>
2022-03-31 18:34:44 +0800

    wrap

--- main.go
+++ main.go
@@ -30,9 +30,9 @@ func rocket(lines []string) {
 	table = append(table, "bypass-system = true")
 	table = append(table, "skip-proxy = 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, localhost, *.local, e.crashlytics.com, captive.apple.com")
 	table = append(table, "bypass-tun = 10.0.0.0/8,100.64.0.0/10,127.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.0.0.0/24,192.0.2.0/24,192.88.99.0/24,192.168.0.0/16,198.18.0.0/15,198.51.100.0/24,203.0.113.0/24,224.0.0.0/4,255.255.255.255/32")
-	table = append(table, "dns-server = system, 114.114.114.114, 112.124.47.27, 8.8.8.8, 8.8.4.4")
-	table = append(table, "[Rule]\n")
+	table = append(table, "dns-server = system, 114.114.114.114, 112.124.47.27, 8.8.8.8, 8.8.4.4\n")
 
+	table = append(table, "[Rule]")
 	mark := make(map[string]struct{}, 0)
 	for _, v := range lines {
 		h1 := v[0:1]
